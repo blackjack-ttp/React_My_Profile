5c00b3743a7e3265a18bfe3e4202d023caee6e9b
blackjack-ttp <phongtt120298@outlook.com>
2023-09-22 13:27:46 +0700

    Fueture home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import "./styles/App.css";
-import { Routes, Route } from "react-router-dom";
+import "./styles/styleApp.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "./containers/home";
 import About from "./containers/about";
 import Resume from "./containers/resume";
@@ -7,23 +9,35 @@ import Skills from "./containers/skills";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
+import particles from "./utils/particles";
 function App() {
+  const location = useLocation();
+
+  const renderParticleJsInHomePage = location.pathname === "/";
+
+  const handleInit = async (main) => {
+    await loadFull(main);
+  };
   return (
     <>
       <div className="App">
         {/* particles js */}
-
+        {renderParticleJsInHomePage && (
+          <Particles id="particles" options={particles} init={handleInit} />
+        )}
         {/* navbar */}
         <Navbar />
         {/* main page content */}
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <div className="App__main-page-content">
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,10 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleNavigeToContactMePage = () => {
+    navigate("/contact");
+  };
   return (
     <>
-      <h1>Home Page</h1>
+      <section id="home" className="home">
+        <div className="home__text-wrapper">
+          <h1>
+            Hello, I'm BlackJack-TTP
+            <br />
+            Web Developer
+          </h1>
+        </div>
+        <Animate
+          play
+          duration={1.5}
+          delay={1}
+          start={{ transform: "translateY(550px)" }}
+          end={{ transform: "translateY(0px)" }}
+        >
+          <div className="home__contact-me">
+            <button onClick={handleNavigeToContactMePage}>Hire Me</button>
+          </div>
+        </Animate>
+      </section>
     </>
   );
 };
