215b3d4bf22c8aee0acc1a00cf2fc34913e3da59
blackjack-ttp <phongtt120298@outlook.com>
2023-10-01 20:46:06 +0700

    #2 [feature function] update function skills component and responsive page skills

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -3,15 +3,56 @@ import React from 'react';
 import { BsInfoCircleFill } from 'react-icons/bs';
 import PageHeaderContent from '@Components/pageHeaderContent';
 import './styles.scss';
+import skillsData from './utils';
+import { Animate, AnimateKeyframes } from 'react-simple-animate';
+import { Line } from 'rc-progress';
 
 const Skills = () => {
   return (
     <>
       <section id="skills" className="skills">
-        <PageHeaderContent
-          headerText="My Skills"
-          icon={<BsInfoCircleFill size={40} />}
-        />
+        <PageHeaderContent headerText="My Skills" icon={<BsInfoCircleFill size={40} />} />
+        <div className="skills__content-wrapper">
+          {skillsData.map((item, i) => (
+            <div key={i} className="skills__content-wrapper__inner-content">
+              <Animate
+                play
+                duration={1}
+                delay={0.3}
+                start={{ transform: 'translateX(-200px)' }}
+                end={{ transform: 'translateX(0px)' }}
+              >
+                <h3 className="skills__content-wrapper__inner-content__categoty-text">
+                  {item.label}
+                </h3>
+                <div className="skills__content-wrapper__inner-content__progressbar-container">
+                  {item.data.map((skillItem, j) => (
+                    <AnimateKeyframes
+                      play
+                      duration={1}
+                      keyframes={['opacity: 1', 'opacity: 0']}
+                      iterationCount="1"
+                    >
+                      <div
+                        className="skills__content-wrapper__inner-content__progressbar-wrapper"
+                        key={j}
+                      >
+                        <p>{skillItem.skillName}</p>
+                        <Line
+                          percent={skillItem.percentage}
+                          strokeWidth="2"
+                          strokeColor="var(--yellow-theme-main-color)"
+                          trailWidth={'2'}
+                          strokeLinecap="square"
+                        />
+                      </div>
+                    </AnimateKeyframes>
+                  ))}
+                </div>
+              </Animate>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   );
---
+++ src/containers/skills/utils.jsx
@@ -0,0 +1,62 @@
+const skillsData = [
+  {
+    label: 'FRONT END',
+    data: [
+      {
+        skillName: 'HTML',
+        percentage: 70,
+      },
+      {
+        skillName: 'SCSS',
+        percentage: 50,
+      },
+      {
+        skillName: 'JAVASCRIPT',
+        percentage: 80,
+      },
+      {
+        skillName: 'REACT JS',
+        percentage: 40,
+      },
+    ],
+  },
+  {
+    label: 'BACK END',
+    data: [
+      {
+        skillName: 'NODE JS',
+        percentage: 50,
+      },
+      {
+        skillName: 'EXPRESS JS',
+        percentage: 50,
+      },
+      // {
+      //   skillName: 'JAVASCRIPT',
+      //   percentage: 80
+      // },
+      // {
+      //   skillName: 'REACT JS',
+      //   percentage: 40
+      // },
+    ],
+  },
+  {
+    label: 'DATABASES',
+    data: [
+      {
+        skillName: 'MONGODB',
+        percentage: 70,
+      },
+      {
+        skillName: 'MYSQL',
+        percentage: 50,
+      },
+      {
+        skillName: 'CLODINARY',
+        percentage: 70,
+      },
+    ],
+  },
+];
+export default skillsData;
